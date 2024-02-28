a96ba589261d7bf15490502bfe838267a117167c
maheen-raza <113572260+maheen-raza@users.noreply.github.com>
2024-02-27 17:48:02 -0700

    mehrnaz fixed button issues

--- yeschef/src/index.js
+++ yeschef/src/index.js
@@ -12,14 +12,10 @@ root.render(
     <Router>
       <Routes>
         {" "}
-        {/* Use Routes instead of div */}
         <Route path="/" element={<WebHome />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   </React.StrictMode>
 );
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
