1395879fc5dc0481a65f57aa1a0bc920e734d560
maheen-raza <113572260+maheen-raza@users.noreply.github.com>
2024-03-17 13:36:01 -0600

    merging

--- yeschef/src/App.js
+++ yeschef/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import WebHome from "./WebHome/WebHome";
-import Login from "./Login/Login";
+import WebHome from "./WebHome/WebHome"; 
+import Login from "./Login/Login"; 
 import CreateAccount from "./CreateAccount/CreateAccount";
 import SettingsPage from "./SettingsPage/SettingsPage"; 
 import Search from "./Search/Search";
 import SearchByTags from "./SearchByTags/SearchByTags"
+import SearchByCategory from "./SearchByCategory/SearchByCategory";
+import SearchByChef from "./SearchByChef/SearchByChef";
+import SearchByIngredients from "./SearchByIngredients/SearchByIngredients";
 
 
 function App() {
@@ -15,6 +18,9 @@ function App() {
         <Route path="/" exact component={WebHome} />
         <Route path="/login" component={Login} />
         <Route path="/createaccount" component={CreateAccount} />
+        <Route path="/searchbycategory" component={SearchByCategory} />
+        <Route path="/searchbychef" component={SearchByChef} />
+        <Route path="/searchbyingredients" component={SearchByIngredients} />
         <Route path="/search" component={Search} />
         <Route path="/settings" component={SettingsPage} />
         <Route path="/searchbytags" component={SearchByTags} />
